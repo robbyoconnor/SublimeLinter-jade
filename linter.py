0f57e8128f615760c1ef88e190823c09c7ccdd38
Pierre Tardy <pierre.tardy@intel.com>
2013-12-17 01:16:36 +0100

    initial commit
    Signed-off-by: Pierre Tardy <pierre.tardy@intel.com>

---
+++ linter.py
@@ -0,0 +1,35 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Pierre Tardy
+# Copyright (c) 2013 Pierre Tardy
+#
+# License: MIT
+#
+"""This module exports the Jade plugin class."""
+
+from SublimeLinter.lint import Linter, util
+
+
+class Jade(Linter):
+
+    """Provides an interface to jade."""
+
+    syntax = ('jade', )
+    executable = 'jade'
+    tempfile_suffix = 'jade'
+    multiline = True
+    regex = r'''(?xi)
+        Error: .+?:(?P<line>\d+).*\r?\n
+        (.*\|.*\n)+
+        .*\n
+        (?P<message>.*)
+    '''
+    error_stream = util.STREAM_STDERR
+
+    def cmd(self):
+        """Return a list with the command line to execute."""
+
+        result = ('jade',)
+        return result
